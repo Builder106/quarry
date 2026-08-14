d3ce821fcd5b0ef2b4d81f73f98c97de784efebe
Builder106 <vaughanolayinka@gmail.com>
2026-08-14 10:57:13 -0700

    feat: add linting and formatting jobs for bot and site, update package.json and configuration files

---
+++ bot/eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        node: true,
+        es2024: true,
+        Bun: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  prettier,
+  {
+    ignores: ['node_modules/', '*.config.*', 'scripts/'],
+  }
+)
---
+++ site/eslint.config.js
@@ -0,0 +1,46 @@
+import js from '@eslint/js'
+import next from 'eslint-plugin-next'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...next.configs.recommended,
+  react.configs.recommended,
+  reactHooks.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        browser: true,
+        es2024: true,
+        node: true,
+        Bun: true,
+      },
+    },
+    settings: {
+      react: { version: '19.2' },
+      next: { rootDir: '.' },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  prettier,
+  {
+    ignores: ['.next/', 'node_modules/', '*.config.*', 'out/'],
+  }
+)
